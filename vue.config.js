82919c1c12711e1f71f515b4f1685dd55479a41a
Song-Gi-Sung <wlswlek123@naver.com>
2024-10-29 15:17:14 +0900

    git init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3000, // 프론트엔드 port
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 url
+        changeOrigin: true, // 원본 요청의 호스트를 타겟 URL로 변경
+      }
+    }
+  },
 })
